ba5ccda623c496fb84260b6b44cb0bd363eaf593
Sadig2488 <sadayfa88@mail.ru>
2021-03-09 09:54:05 +0300

    4. Загрузите файл Multiple.java на сайт github и оставьте ссылку на изменения.

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Sadig Badalov");
+        System.out.println("09.03.2021");
+    }
+}
