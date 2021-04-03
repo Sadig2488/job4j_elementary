79da37cd746effe30ee530264f1f0e28aa71acc1
Sadig2488 <sadayfa88@mail.ru>
2021-04-03 13:36:12 +0300

    1. Модульные тесты. [#490196]

--- test/ru/job4j/ru/job4j/ConverterTest.java
+++ test/ru/job4j/ru/job4j/ConverterTest.java
@@ -10,7 +10,8 @@ public class ConverterTest {
     @Test
     public void whenConvert140RblThen2Euro() {
         int in = 140;
-        int expected = 2;
+        int expected;
+        expected = 2;
         int out = Converter.rubleToEuro(in);
         Assert.assertEquals(expected, out);
 
@@ -21,7 +22,8 @@ public class ConverterTest {
 
         int in = 180;
         int expected = 3;
-        int out = Converter.rubleToDollar(in);
+        int out;
+        out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
 
     }
