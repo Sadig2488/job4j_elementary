ddbe13f16280729c82f0f164b117ac8fb689eb2f
Sadig2488 <sadayfa88@mail.ru>
2021-04-03 13:33:33 +0300

    1. Модульные тесты. [#490196]

--- src/ru/job4j/ru/job4j/Converter.java
+++ src/ru/job4j/ru/job4j/Converter.java
@@ -7,15 +7,15 @@ public class Converter {
             return rsl;
         }
 
-        public static double rubleToDollar(double value) {
-            double rsl = value / 60;
+        public static int rubleToDollar(int value) {
+            int rsl = value / 60;
             return rsl;
         }
 
         public static void main(String[] args) {
 
-            int in = 120;
-            double expected = 2.0;
+            int in = 180;
+            double expected = 3.0;
             double out = Converter.rubleToDollar(in);
             boolean passed = expected == out;
             System.out.println("140 rubles are 2.0 Test result : " + passed);
@@ -23,7 +23,7 @@ public class Converter {
 
             int euro = Converter.rubleToEuro(140);
             System.out.println("140 rubles are " + euro + " euro.");
-            double dollar = Converter.rubleToDollar(120);
+            int dollar = Converter.rubleToDollar(180);
             System.out.println("140 rubles are " + dollar + " dollar.");
         }
     }
---
+++ test/ru/job4j/ru/job4j/ConverterTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+
+    }
+
+    @Test
+    public void whenConvert180RblThen3Dollars() {
+
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+
+    }
+}
