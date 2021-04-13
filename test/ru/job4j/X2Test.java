6a036e6784589457c8f752a157eda7bb3e99b4bf
sadig2488 <adig88@mail.ru>
2021-04-13 21:16:10 +0300

    1.1. Входные данные в тесте [#490201]

--- test/ru/job4j/X2Test.java
+++ test/ru/job4j/X2Test.java
@@ -8,19 +8,18 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenA1B1C1X1Then3() {
-        /* Входные параметры. Их будет 4 штуки */
-        int a = 1;
+    public void whenA0B1C1X1Then2() {
+
+
+        int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        /* ожидаемое значение. Это всегда одна переменная. */
-        int expected = 3;
-        /* вызов метода, который хотим проверить. */
-        /* Результат вычисления. Это всегда одна переменная. */
+
+        int expected = 2;
+
 
         int rsl = X2.calc(a, b, c, x);
-        /* сравнение полученного значения с ожидаемым */
         Assert.assertEquals(expected, rsl);
     }
 }
