acf543b64f99592f5a90eb565745812efe327b6a
sadig2488 <adig88@mail.ru>
2021-04-13 21:16:54 +0300

    1.1. Входные данные в тесте [#490201]

--- test/ru/job4j/X2Test.java
+++ test/ru/job4j/X2Test.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.*;
 public class X2Test {
 
     @Test
-    public void whenA0B1C1X1Then2() {
+    public void whenA1B1C0X1Then2() {
 
 
-        int a = 0;
+        int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
 
         int expected = 2;
